fdfe90f7108114ce493102732437c72f877d00f0
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-06 16:24:57 -0500

    Pesudoclassical example complete

--- Pseudoclassical/example.js
+++ Pseudoclassical/example.js
@@ -1,11 +1,5 @@
 //Pseudoclassical.
 
-Dog.prorotype.bark = function() {
-		return this.name + ' says...BARK!!';
-};
-Dog.prototype.getLegs = function() {
-		return this.legs;
-};
 
 // Constructor function.
 var Dog = function(name) {
@@ -14,15 +8,22 @@ var Dog = function(name) {
 	this.legs = 4;
 	//return this
 };
+// Attaching methods to the Dog prototype.
+Dog.prototype.bark = function() {
+		return this.name + ' says...BARK!!';
+};
+Dog.prototype.getLegs = function() {
+		return this.legs;
+};
 
 
 
-var spot = Dog('Spot');
+var spot = new Dog('Spot');
 spot.bark() // => 'Spot says...BARK!!'
-var rover = Dog('Rover');
+var rover = new Dog('Rover');
 rover.bark() //=> 'Rover says...BARK!!'
 
-// Can change methods for dog by editing the dogMethods object.
+// Can change methods for dog by editing the Dog prototype.
 // EX:
 Dog.prototype.bark = function() {
 	return 'I changed via prototypal link'
@@ -32,5 +33,5 @@ rover.bark() // now returns => 'I changed via prototypal link'
 
 
 // In this case, both spot and rover have access to the bark and getLegs functions via prototype. 
-// Every time Dog is called, a new prototypal link to dogMethods will be created.
+// Every time Dog is called a link to Dog.prototype will be created. 
 // spot.bark === rover.bark => returns true.
